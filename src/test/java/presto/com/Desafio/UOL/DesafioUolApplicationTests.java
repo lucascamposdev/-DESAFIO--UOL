30334b4fcd19963ffa1e11a6b046b26a3296fc8a
Lucas <lucaskmps@gmail.com>
2024-05-06 13:37:33 -0300

    first commit

---
+++ src/test/java/presto/com/Desafio/UOL/DesafioUolApplicationTests.java
@@ -0,0 +1,13 @@
+package presto.com.Desafio.UOL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
