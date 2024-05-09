3c533ebc978502229e81081b973630ca175ca889
Lucas <prestoinker@gmail.com>
2024-05-09 16:42:01 -0300

    Adds DocumentBuilder @Bean to AppConfig so i can inject in CodenameService

--- src/main/java/presto/com/Desafio/UOL/infra/AppConfig.java
+++ src/main/java/presto/com/Desafio/UOL/infra/AppConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 @Configuration
 public class AppConfig {
 
@@ -12,5 +16,10 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public DocumentBuilder documentBuilder() throws ParserConfigurationException {
+        return DocumentBuilderFactory.newInstance().newDocumentBuilder();
     }
 }
